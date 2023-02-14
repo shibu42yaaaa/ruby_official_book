55ef7b14d8cfb793da0b33a14e5ec1e20b9f4c34
shibu42yaaaa <shibu.4242git.formac@gmail.com>
2023-02-14 16:57:40 +0900

    mod

--- practice/section3.rb
+++ practice/section3.rb
@@ -1,33 +1,33 @@
 # 配列
 
-# v1 = %w(hoge foo bar)
+v1 = %w(hoge foo bar)
 
-# p v1.join()     # "hogefoobar"
-# p v1.join("_")  # "hoge_foo_bar"
+p v1.join()     # "hogefoobar"
+p v1.join("_")  # "hoge_foo_bar"
 
 # ---
-# p a = Array.new(5)
-# p a.length
-# # 配列の要素が２。0番目を10とする
-# p Array.new(2){ |index| index + 10 }
+p a = Array.new(5)
+p a.length
+# 配列の要素が２。0番目を10とする
+p Array.new(2){ |index| index + 10 }
 
-# # [nil, nil, nil, nil, nil]
-# # 5
-# # [10, 11]
+# [nil, nil, nil, nil, nil]
+# 5
+# [10, 11]
 
 # -----
-# # 配列の要素は２つ。aとする 
-# a = Array.new(2, "a")
-# # 要素0をbに置き換える
-# p a[0].replace("b")
-# p a
+# 配列の要素は２つ。aとする 
+a = Array.new(2, "a")
+# 要素0をbに置き換える
+p a[0].replace("b")
+p a
 
 # ----
 # ブロックで初期化すると上記のようにはならない
 # 0番目だけを置き換える
-# a = Array.new(2){"a"}
-# a[0].replace("b")
-# p a # ["b", "a"]
+a = Array.new(2){"a"}
+a[0].replace("b")
+p a # ["b", "a"]
 
 # ---------
 v1 = [10,nil,nil,"aa"]
